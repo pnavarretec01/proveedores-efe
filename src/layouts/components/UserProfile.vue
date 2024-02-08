ae2f496a11796121aaccbeae5785285b94d9251b
Pnavarretec <patricionavarrete.c01@gmail.com>
2024-02-08 15:34:18 -0300

    correcciones

--- src/layouts/components/UserProfile.vue
+++ src/layouts/components/UserProfile.vue
@@ -1,68 +1,59 @@
 <script setup>
-import { getCurrentInstance } from 'vue';
-import avatar1 from '@images/avatars/avatar-1.png';
+const keycloak = inject("keycloak");
+import avatar1 from "@images/avatars/avatar-1.png";
 
 const dataUser = JSON.parse(localStorage.getItem("userData"));
-
-const instance = getCurrentInstance();
+const username = keycloak?.tokenParsed?.given_name || "Invitado";
+const rol = keycloak?.tokenParsed?.realm_access?.roles[0] || "Checklist";
 
 async function logout() {
   try {
     localStorage.removeItem("userInfo");
     localStorage.removeItem("userData");
 
-    if (instance && instance.proxy.$keycloak) {
-      let logoutOptions = { redirectUri: window.location.origin };
-      await instance.proxy.$keycloak.logout(logoutOptions);
-    }
+    let logoutOptions = { redirectUri: window.location.origin };
+    await keycloak.logout(logoutOptions);
   } catch (error) {
     console.error(error);
   }
 }
-
 </script>
 
-
 <template>
-  <VBadge dot location="bottom right" offset-x="3" offset-y="3" bordered color="success">
+  <VBadge
+    dot
+    location="bottom right"
+    offset-x="3"
+    offset-y="3"
+    bordered
+    color="success"
+  >
     <VAvatar class="cursor-pointer" color="primary" variant="tonal">
       <VImg :src="avatar1" />
-      <!-- SECTION Menu -->
       <VMenu activator="parent" width="230" location="bottom end" offset="14px">
         <VList>
-          <!-- 👉 User Avatar & Name -->
           <VListItem>
             <template #prepend>
               <VListItemAction start>
-                <VBadge dot location="bottom right" offset-x="3" offset-y="3" color="success">
+                <VBadge
+                  dot
+                  location="bottom right"
+                  offset-x="3"
+                  offset-y="3"
+                  color="success"
+                >
                   <VAvatar color="primary" variant="tonal">
                     <VImg :src="avatar1" />
                   </VAvatar>
                 </VBadge>
               </VListItemAction>
             </template>
-
             <VListItemTitle class="font-weight-semibold">
-              {{ dataUser.nombre_usuario }}
+              {{ username }}
             </VListItemTitle>
-            <VListItemSubtitle>{{ dataUser.rol_usuario }}</VListItemSubtitle>
+            <VListItemSubtitle>{{ rol }}</VListItemSubtitle>
           </VListItem>
-
           <VDivider class="my-2" />
-
-          <!-- 👉 Profile -->
-          <VListItem link>
-            <template #prepend>
-              <VIcon class="me-2" icon="tabler-user" size="22" />
-            </template>
-
-            <VListItemTitle>Perfil</VListItemTitle>
-          </VListItem>
-
-          <!-- Divider -->
-          <VDivider class="my-2" />
-
-          <!-- 👉 Logout -->
           <VListItem @click="logout">
             <template #prepend>
               <VIcon class="me-2" icon="tabler-logout" size="22" />
@@ -71,7 +62,6 @@ async function logout() {
           </VListItem>
         </VList>
       </VMenu>
-      <!-- !SECTION -->
     </VAvatar>
   </VBadge>
 </template>
